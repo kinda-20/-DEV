d90467904e0382d91ed1ed40364c9ce13f75e3bc
kinda-20 <150470393+kinda-20@users.noreply.github.com>
2023-11-15 16:30:27 +0300

    Add files via upload

---
+++ month.cs
@@ -0,0 +1,42 @@
+﻿byte Ay = Convert.ToByte(Console.ReadKey());
+switch (Ay)
+{
+    case 1:
+        Console.WriteLine("Ocak");
+        break;
+    case 2:
+        Console.WriteLine("Şubat");
+        break;
+    case 3:
+        Console.WriteLine("Mart");
+        break;
+    case 4:
+        Console.WriteLine("Nisan");
+        break;
+    case 5:
+        Console.WriteLine("Mayıs");
+        break;
+    case 6:
+        Console.WriteLine("Haziran");
+        break;
+    case 7:
+        Console.WriteLine("Temmuz");
+        break;
+    case 8:
+        Console.WriteLine("Ağustos");
+        break;
+    case 9:
+        Console.WriteLine("Eylül");
+        break;
+    case 10:
+        Console.WriteLine("Ekim");
+        break;
+    case 11:
+        Console.WriteLine("Kasım");
+        break;
+    case 12:
+        Console.WriteLine("Aralık");
+        break;
+}
+
+
